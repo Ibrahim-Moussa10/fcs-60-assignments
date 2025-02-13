7e644fcb64b5f95efa04a79f927a31c5442350d5
Ibrahim-Moussa10 <i.moussa.14.im@gmail.com>
2025-02-13 17:58:47 +0200

    solving exercise #2 in 3 different ways

--- Assignment-FCS.py
+++ Assignment-FCS.py
@@ -1,13 +1,37 @@
 #Exercise 1
-age = int(input("Enter your age: "))
-if age < 0:
-    print( "invalid input")
-if age < 3:
-    print( "Free")
-elif age >= 3 and age <= 12:
-    print( "$10")
-elif age >= 13 and age <= 59:
-    print( "$15")
-elif age >= 60:
-    print( "$8")
+# age = int(input("Enter your age: "))
+# if age < 0:
+#     print( "invalid input")
+# if age < 3:
+#     print( "Free")
+# elif age >= 3 and age <= 12:
+#     print( "$10")
+# elif age >= 13 and age <= 59:
+#     print( "$15")
+# elif age >= 60:
+#     print( "$8")
 
+#Exercise 2-1
+
+# number = int(input("Enter a number: "))
+
+# if number //2 == number / 2 :
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Exercise 2-2
+
+# number = int(input("Enter a number: "))
+# if number %2 == 0 :
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Exercise 2-3
+
+number = int(input("Enter a number: "))
+if number & 1 == 0:
+    print("Even")
+else:
+    print("Odd")
